3e3f4f836f3215637d912c176b8cd2a9a5422846
razaaasif <aasifraza9123@gmail.com>
2026-03-22 21:50:02 +0530

    HT-2 Add Flyway Config and Migration #2

---
+++ src/main/resources/db/migration/V20260322_01__init_schema.sql
@@ -0,0 +1,6 @@
+-- =====================================================
+-- SCHEMA: health_tracker
+-- =====================================================
+
+CREATE SCHEMA IF NOT EXISTS health_tracker;
+SET search_path TO health_tracker;
