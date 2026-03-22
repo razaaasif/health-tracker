bc21ccd12377d38a64e5db5c966566cde518a2e9
razaaasif <aasifraza9123@gmail.com>
2026-03-22 21:34:31 +0530

    #1 initial project setup

---
+++ src/main/java/com/raza/healthtracker/HealthTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.raza.healthtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthTrackerApplication.class, args);
+    }
+
+}
